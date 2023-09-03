73d99978a99c3c4c4c23aba53e81c34b63a45438
heslin <73648326+dyhes@users.noreply.github.com>
2023-09-03 14:16:41 +0800

    check

--- backend-test-server/server.js
+++ backend-test-server/server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,33 +9,30 @@ const port = 8080;
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'http://127.0.0.1:5173');
+    res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
-  });
-// const sslOptions = {
-//     key: fs.readFileSync('key.pem'),
-//     cert: fs.readFileSync('cert.pem'),
-//     ciphers: [
-//         "ECDHE-RSA-AES128-SHA256",
-//         "DHE-RSA-AES128-SHA256",
-//         "AES128-GCM-SHA256",
-//         "RC4",
-//         "HIGH",
-//         "!MD5",
-//         "!aNULL"
-//     ].join(':'),
-// };
+});
 
 // Sample data
 const users = [
     {
+        id: 1,
         username: 'Jay',
         password: '123456',
         email: 'jay@mail.com'
     }
 ];
+
+// JWT secret key
+const jwtSecret = 'your-secret-key';
+
+// Generate a random verification code
+function generateVerificationCode() {
+    return Math.floor(1000 + Math.random() * 9000).toString();
+}
+const verificationCodes = {};
 
 // Login API endpoint
 app.post('/users/login', (req, res) => {
@@ -42,34 +40,91 @@ app.post('/users/login', (req, res) => {
 
     const user = users.find(user => user.username === username && user.password === password);
     if (user) {
+        // Create a JWT token
+        const token = jwt.sign({ username: user.username }, jwtSecret, { expiresIn: '1h' });
         res.json({
             code: true,
             msg: 'success',
-            data: '111'
+            data: {
+                token: token
+            }
         });
     } else {
         res.json({
             code: false,
             msg: 'invalid credentials',
-            data: '111'
+            data: null
         });
     }
 });
 
 // Signup API endpoint
 app.post('/users/signup', (req, res) => {
-    const { username, password, email } = req.body;
+    const { username, password } = req.body;
 
     // Simplified signup 
-    users.push({
+    const newUser = {
+        id: users.length + 1,
         username,
-        password,
-        email
-    });
+        password
+    };
+    users.push(newUser);
 
     res.json({
         code: true,
         msg: 'success',
+        data: {
+            user: newUser
+        }
+    });
+});
+
+// Send verification code API endpoint
+app.post('/users/forgetPassword', (req, res) => {
+    const { email, username } = req.body;
+
+    // Find the user by username (you should use a database for this)
+    const user = users.find(user => user.username === username && user.email === email);
+    if (!user) {
+        res.json({
+            code: false,
+            msg: 'user not found',
+            data: null
+        });
+        return;
+    }
+
+    const verificationCode = generateVerificationCode();
+
+    verificationCodes[user.id] = verificationCode;
+
+    res.json({
+        code: true,
+        msg: 'verification code sent',
+        data: verificationCode
+    });
+});
+
+// Verify verification code and update password API endpoint
+app.post('/users/updatePassword', (req, res) => {
+    const { id, newPassword } = req.body;
+
+    const storedVerificationCode = verificationCodes[id];
+    if (!storedVerificationCode) {
+        res.json({
+            code: false,
+            msg: 'invalid verification code',
+            data: null
+        });
+        return;
+    }
+    const userIndex = users.findIndex(user => user.id === id);
+    if (userIndex !== -1) {
+        users[userIndex].password = newPassword;
+    }
+    res.json({
+        code: true,
+        msg: 'password updated',
         data: null
     });
 });
